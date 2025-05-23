96cb706106002fdee3d45566123a380baa5c07c4
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-05-23 11:46:35 -0400

    Add transaction table and refactor form to use FormData API

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import "./App.css";
+import { useState } from "react";
+import formatTransaction from "./helpers/formatTransaction";
+
+type emanTransaction = {
+  transaction: number;
+  type: string;
+};
 
 function App() {
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
+  const [transactions, setTransaction] = useState<emanTransaction[]>([]);
 
-    const form: HTMLFormElement = event.currentTarget;
-    const transactionAmount = form.elements.namedItem(
-      "transactionAmount",
-    ) as HTMLInputElement;
-    const transactionType = form.elements.namedItem(
-      "transactionType",
-    ) as HTMLInputElement;
+  function postTransaction(formData: FormData) {
+    const transactionAmount = formData.get("transactionAmount");
+    const transactionType = formData.get("transactionType");
 
-    console.log(transactionAmount.value, transactionType.value);
+    const newTransaction: emanTransaction = {
+      transaction: Number(transactionAmount),
+      type: transactionType as string,
+    };
 
-    transactionAmount.value = "";
-    transactionType.value = transactionType.defaultValue;
+    setTransaction([...transactions, newTransaction]);
+    formData.delete("transactionAmount");
+    formData.delete("transactionType");
   }
   return (
     <>
       <h1>Budgeting Web Application</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form action={postTransaction}>
         <label htmlFor="transactionAmount">
           Transaction Amount
           <input
@@ -33,12 +39,39 @@ function App() {
         <label htmlFor="transactionType">
           Transaction Type
           <select id="transactionType" name="transactionType">
-            <option value="income">Income</option>
-            <option value="expense">Expense</option>
+            <option value="Income">Income</option>
+            <option value="Expense">Expense</option>
           </select>
         </label>
         <input type="submit" value="Log" />
       </form>
+
+      <table border={1}>
+        <thead>
+          <th>Transactions</th>
+          <th>Transaction Type</th>
+        </thead>
+        <tbody>
+          {transactions.length != 0 ? (
+            transactions.map((transaction) => (
+              <>
+                <tr>
+                  <td>$
+                    {transaction.transaction.toLocaleString("en-US", {
+                      minimumFractionDigits: 2,
+                    })}
+                  </td>
+                  <td>{transaction.type}</td>
+                </tr>
+              </>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={2}>Add a Transaction to display them here!</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </>
   );
 }
