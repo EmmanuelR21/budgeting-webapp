491b459e03d9e781a73ee46f66305076752a6504
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-05-24 09:47:46 -0400

    Add transaction description field

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 type emanTransaction = {
   transaction: number;
   type: string;
+  description: string;
 };
 
 function App() {
@@ -12,21 +13,27 @@ function App() {
   function postTransaction(formData: FormData) {
     const transactionAmount = formData.get("transactionAmount");
     const transactionType = formData.get("transactionType");
+    const transactionDescription = formData.get("transactionDescription");
 
     const newTransaction: emanTransaction = {
       transaction: Number(transactionAmount),
       type: transactionType as string,
+      description: transactionDescription as string,
     };
 
     setTransaction([...transactions, newTransaction]);
     formData.delete("transactionAmount");
     formData.delete("transactionType");
+    formData.delete("transactionDescription");
   }
   return (
     <>
       <h1>Budgeting Web Application</h1>
 
-      <form action={postTransaction}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        action={postTransaction}
+      >
         <label htmlFor="transactionAmount">
           Transaction Amount
           <input
@@ -38,9 +45,20 @@ function App() {
         <label htmlFor="transactionType">
           Transaction Type
           <select id="transactionType" name="transactionType">
+            <option selected value="General">
+              General
+            </option>
             <option value="Income">Income</option>
             <option value="Expense">Expense</option>
           </select>
+        </label>
+        <label htmlFor="transactionDescription">
+          Description
+          <input
+            id="transactionDescription"
+            name="transactionDescription"
+            type="text"
+          />
         </label>
         <input type="submit" value="Log" />
       </form>
@@ -49,24 +67,27 @@ function App() {
         <thead>
           <th>Transactions</th>
           <th>Transaction Type</th>
+          <th>Description</th>
         </thead>
         <tbody>
           {transactions.length != 0 ? (
             transactions.map((transaction) => (
               <>
                 <tr>
-                  <td>$
+                  <td>
+                    $
                     {transaction.transaction.toLocaleString("en-US", {
                       minimumFractionDigits: 2,
                     })}
                   </td>
                   <td>{transaction.type}</td>
+                  <td>{transaction.description}</td>
                 </tr>
               </>
             ))
           ) : (
             <tr>
-              <td colSpan={2}>Add a Transaction to display them here!</td>
+              <td colSpan={3}>Add a Transaction to display them here!</td>
             </tr>
           )}
         </tbody>
